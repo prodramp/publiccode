52f9087e378fc8b8bf109222e9a7172316619353
Avkash Chauhan <avkash@MacBook-Pro.local>
2022-03-13 22:30:32 -0700

    update to nivo coreui

---
+++ coreui/coreui-nivo-graphs/coreui-nivo-final/src/index.js
@@ -0,0 +1,20 @@
+import 'react-app-polyfill/stable'
+import 'core-js'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { Provider } from 'react-redux'
+import store from './store'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
